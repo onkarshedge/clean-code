1d206f21f4364c72936e56dcab7d3b4b71287fd7
onkar shedge <shedge31onkar@gmail.com>
2018-12-17 12:25:22 +0530

    Add test before adding functionality

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,9 +1,28 @@
 package com.thoughtworks.movierental;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void testGenerateStatement() {
+        final Customer customer = new Customer("Onkar");
+        final Movie avengers = new Movie("Avengers", Movie.CHILDRENS);
+        final Movie lionKing = new Movie("lion King", Movie.NEW_RELEASE);
+        final Movie robot = new Movie("Robot", Movie.REGULAR);
+        final Rental avengesRental = new Rental(avengers, 4);
+        final Rental lionKingrental = new Rental(lionKing, 2);
+        final Rental robotRental = new Rental(robot, 2);
+        customer.addRental(avengesRental);
+        customer.addRental(lionKingrental);
+        customer.addRental(robotRental);
+        Assert.assertEquals("Rental Record for Onkar\n" +
+                "\tAvengers\t3.0\n" +
+                "\tlion King\t6.0\n" +
+                "\tRobot\t2.0\n" +
+                "Amount owed is 11.0\n" +
+                "You earned 4 frequent renter points", customer.statement());
+
+    }
 
 }
